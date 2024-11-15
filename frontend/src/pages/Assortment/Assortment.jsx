f751ac86a058015c35b7600f35b1015851f3e462
n1tr0x0 <isabaevali78@gmail.com>
2024-11-16 01:10:19 +0300

    assotment

--- frontend/src/pages/Assortment/Assortment.jsx
+++ frontend/src/pages/Assortment/Assortment.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProducts } from "../../store/reducers/products/products";
 import { Link } from "react-router-dom";
+import "./Assortment.scss";
 
 const Assortment = () => {
   const { data, status, error } = useSelector((state) => state.products);
@@ -12,11 +13,23 @@ const Assortment = () => {
   }, [dispatch]);
 
   return (
-    <div>
+    <div className="Assortment">
       {data.map((item) => (
-        <div key={item.id}>
-          <h2>{item.title}</h2>
+        <a href="#">
+          <div className="Assortment__cart" key={item.id}>
+          <img
+            className="Assortment__cart_image"
+            src={item.image}
+            alt="ПОСТЕР ВРЕМЕННО ОТСУТСВУЕТ"
+          />
+          <h2 className="Assortment__cart_title">{item.title}</h2>
+          <a href="" className="Assortment__cart_cotegory">{item.category}</a>
+          <p className="Assortment__cart_subtitle">{item.description}</p>
+
+          <p className="Assortment__cart_price">{item.price} $</p>
         </div>
+        </a>
+        
       ))}
     </div>
   );
